d89a9ce6b10bb7d7412590904a1badb81d86c585
Chip Office <chipoffice@Chips-iMac.local>
2019-01-30 12:25:08 -0500

    MainTabNavigator

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -50,7 +50,7 @@ SettingsStack.navigationOptions = {
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+      name={Platform.OS === 'ios' ? 'ios-camera' : 'md-options'}
     />
   ),
 };
