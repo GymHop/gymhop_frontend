ee1c7c6be47b6585fb9519abd96b831ef149e455
David Axelrod <daxaxelrod@gmail.com>
2019-03-20 19:24:24 -0400

    fixed the phone number length problem as well as gymhop_frontend being the name of the app for ios

--- screens/register/PopulateUserProfileScreen.js
+++ screens/register/PopulateUserProfileScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { View, Text, Image, Platform, Keyboard,
+import { View, Text, Image, Platform, Keyboard, TouchableWithoutFeedback,
   TextInput, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
@@ -56,7 +56,7 @@ class RegisterPart2 extends React.Component {
   };
 
   handlePhoneInput = (text) => {
-    if (text.length > 10) {
+    if (text.length > 9) {
       Keyboard.dismiss();
     }
     this.setState({phone: text});
@@ -70,69 +70,71 @@ class RegisterPart2 extends React.Component {
 
     // let selectedYear = this.state.birthday.getFullYear();
     return (
-      <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios'?'padding':null} enabled>
-        <Image
-          source={require('../../assets/images/loginheader.png')}
-          style={styles.headLogo}
-          resizeMode='contain'
-      />
-      <TextInput
-        style={styles.registerInput}
-        placeholder={'First Name'}
-        placeholderTextColor={'#8f8f8f'}
-        onChangeText={(text) => this.setState({first_name: text})}
-      />
-      <TextInput
-        style={styles.registerInput}
-        placeholder={'Last Name'}
-        placeholderTextColor={'#8f8f8f'}
-        onChangeText={(text) => this.setState({last_name: text})}
-      />
-      <TextInput
-        style={styles.registerInput}
-        placeholder={'Cell phone number'}
-        inlineImageLeft='phone'
-        keyboardType={'phone-pad'}
-        placeholderTextColor={'#8f8f8f'}
-        onChangeText={(text) => {this.handlePhoneInput(text)})}
-      />
-      {/*<PhotoUploadForm onPhotoSelected={(pic)=>this.setState({profile_pic: pic})}/>*/}
-      {/*<Text style={{color:"white"}}>Photo upload to be done after project ejection</Text>*/}
-      <Text style={[styles.whiteText, {marginTop: 10}]}>Enter your birthday</Text>
-      <View style={{marginBottom:22}}>
-        <BirthdayPicker
-          styles={[styles.registerInput, styles.registerBirthdayPicker]}
-          lightmode
-          selectedYear={year}
-          selectedMonth={month}
-          selectedDay={day}
-          onYearValueChange={(year,i) => {
-            let bDay = new Date(this.state.birthday);
-            bDay.setFullYear(year);
-            this.setState({birthday: bDay}) // never use the same instance of mutated state in setState
-          }}
-          onMonthValueChange={(month,i) => {
-            let bDay = new Date(this.state.birthday);
-            bDay.setMonth(month);
-            this.setState({birthday: bDay})
-          }}
-          onDayValueChange={(day,i) => {
-            let bDay = new Date(this.state.birthday);
-            bDay.setDate(day);
-            this.setState({birthday: bDay})
-          }}
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios'?'padding':null} enabled>
+          <Image
+            source={require('../../assets/images/loginheader.png')}
+            style={styles.headLogo}
+            resizeMode='contain'
         />
-      </View>
-      <TouchableOpacity style={styles.registerButton} onPress={() => {this.registerUser()}}>
-        <Text style={styles.registerText}>Register</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.loginButton} onPress={() => {
-            this.props.navigation.pop()
-        }}>
-        <Text style={styles.loginText}>Back</Text>
-      </TouchableOpacity>
+        <TextInput
+          style={styles.registerInput}
+          placeholder={'First Name'}
+          placeholderTextColor={'#8f8f8f'}
+          onChangeText={(text) => this.setState({first_name: text})}
+        />
+        <TextInput
+          style={styles.registerInput}
+          placeholder={'Last Name'}
+          placeholderTextColor={'#8f8f8f'}
+          onChangeText={(text) => this.setState({last_name: text})}
+        />
+        <TextInput
+          style={styles.registerInput}
+          placeholder={'Cell phone number'}
+          inlineImageLeft='phone'
+          keyboardType={'phone-pad'}
+          placeholderTextColor={'#8f8f8f'}
+          onChangeText={(text) => {this.handlePhoneInput(text)}}
+        />
+        {/*<PhotoUploadForm onPhotoSelected={(pic)=>this.setState({profile_pic: pic})}/>*/}
+        {/*<Text style={{color:"white"}}>Photo upload to be done after project ejection</Text>*/}
+        <Text style={[styles.whiteText, {marginTop: 10}]}>Enter your birthday</Text>
+        <View style={{marginBottom:22}}>
+          <BirthdayPicker
+            styles={[styles.registerInput, styles.registerBirthdayPicker]}
+            lightmode
+            selectedYear={year}
+            selectedMonth={month}
+            selectedDay={day}
+            onYearValueChange={(year,i) => {
+              let bDay = new Date(this.state.birthday);
+              bDay.setFullYear(year);
+              this.setState({birthday: bDay}) // never use the same instance of mutated state in setState
+            }}
+            onMonthValueChange={(month,i) => {
+              let bDay = new Date(this.state.birthday);
+              bDay.setMonth(month);
+              this.setState({birthday: bDay})
+            }}
+            onDayValueChange={(day,i) => {
+              let bDay = new Date(this.state.birthday);
+              bDay.setDate(day);
+              this.setState({birthday: bDay})
+            }}
+          />
+        </View>
+        <TouchableOpacity style={styles.registerButton} onPress={() => {this.registerUser()}}>
+          <Text style={styles.registerText}>Register</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.loginButton} onPress={() => {
+              this.props.navigation.pop()
+          }}>
+          <Text style={styles.loginText}>Back</Text>
+        </TouchableOpacity>
 
-      </KeyboardAvoidingView>
+        </KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
     )
   }
 }
