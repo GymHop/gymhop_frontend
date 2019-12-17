e3dc12d41c9951c0d920bafa7bf8f97b32434b04
David Axelrod <daxaxelrod@gmail.com>
2019-12-16 20:39:52 -0500

    Animated payment selection cards

--- components/payments/SelectableCard.js
+++ components/payments/SelectableCard.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'
+import { View, Text, StyleSheet,
+         TouchableOpacity, Image, Animated } from 'react-native';
+import Layout from '../../constants/Layout';
 
 class SelectableCard extends Component {
   constructor(props) {
     super(props);
+    this.animatedTopValue = new Animated.Value(0);
   }
 
   animateSelected = () => {
     console.log("animateSelected");
+    Animated.spring(this.animatedTopValue, {
+      toValue: 40
+    }).start()
   }
   animateUnselect = () => {
     console.log("animateUnselect");
+    Animated.spring(this.animatedTopValue, {
+      toValue: 0
+    }).start()
   }
 
   componentDidUpdate(prevProps) {
@@ -24,23 +33,69 @@ class SelectableCard extends Component {
     }
   }
 
+  getDateStr = (date) => {
+    var mm = date.getMonth() + 1;
+    var dd = date.getDate();
+
+    return [(mm>9 ? '' : '0') + mm,
+            "/",
+            (dd>9 ? '' : '0') + dd,
+            "/",
+            (date.getFullYear() + "").slice(2),
+           ].join('');
+   };
+
+  getAutoRenewDate = (period) => {
+    var today = new Date();
+    switch (period) {
+      case "month":
+        var nextMonth = new Date(today.getFullYear(), today.getMonth()+1, today.getDate());
+        return this.getDateStr(nextMonth);
+      case "week":
+        var nextweek = new Date(today.getFullYear(), today.getMonth(), today.getDate()+7);
+        return this.getDateStr(nextweek);
+      default:
+    }
+  }
+
   render() {
+    let animatedTop = this.animatedTopValue;
+
     return (
-      <TouchableOpacity onPress={this.props.onSelect}>
-          <Text>{this.props.price}</Text>
-          <Text>{this.props.period}</Text>
-          <Text>{this.props.description}</Text>
-          <Text>selected: {this.props.selected ? "selected" : ""}</Text>
-          <Image style={styles.backgroundImage} source={this.props.image.uri} />
-      </TouchableOpacity>
+      <Animated.View style={[{backgroundColor: this.props.background, top: animatedTop}, styles.container]}>
+        <TouchableOpacity onPress={this.props.onSelect}>
+              {this.props.icon}
+              <Text style={styles.titleText}>{this.props.title}</Text>
+              <Text style={styles.subTitle}>{this.props.price}/{this.props.period}</Text>
+              <Text>Auto Renews: {this.getAutoRenewDate(this.props.period)}</Text>
+              <Text>selected: {this.props.selected ? "selected" : ""}</Text>
+        </TouchableOpacity>
+      </Animated.View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  backgroundImage: {
-    height: 50,
-    width: 50
+  container: {
+    position: "relative",
+    width: Layout.window.width * .8 / 2,
+    marginHorizontal: Layout.window.width *.01,
+    padding: Layout.window.width *.03,
+    borderRadius: 8,
+    shadowColor: "#000",
+    shadowOffset: {
+    	width: 0,
+    	height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
+    color: "#F2F2F2"
+  },
+  overlayContainer: {
+    position: "relative",
+    display: "flex",
+    zIndex: 1
   }
 })
 
--- screens/PaymentScreen.js
+++ screens/PaymentScreen.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 
 import { View, Text, StyleSheet, Button } from 'react-native';
 import { connect } from 'react-redux';
+import Layout from '../constants/Layout';
+import Icon from 'react-native-vector-icons/AntDesign'
 
-import SelectableCard from '../components/payments/SelectableCard'
+import SelectableCard from '../components/payments/SelectableCard';
 
 const METHOD_DATA = [{
   supportedMethods: ['apple-pay', 'google-pay'],
@@ -28,7 +30,9 @@ class PaymentScreen extends Component {
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         image: {
           uri: require("../assets/images/monthly_photo.jpg")
-        }
+        },
+        background: "#BFBFBF",
+        icon: <Icon name="idcard" size={30} color="#000000" />
       },
       {
         price: 20,
@@ -36,38 +40,64 @@ class PaymentScreen extends Component {
         description: " Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         image: {
           uri: require("../assets/images/weekly_photo.jpg")
-        }
+        },
+        background: "#BFBFBF",
+        icon: <Icon name="carryout" size={30} color="#000000" />
       }
     ]
   }
 
   static navigationOptions = {
-    header: null,
-  };
+    headerStyle: {
+            backgroundColor: '#000000',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+    };
 
   openNativePurchaseOption = () => {
     console.log("purchasing starting with option " + this.state.selectedOption + " selected");
   }
 
   render() {
+    var description = this.state.selectedOption != null ?
+      this.paymentOptions[this.state.selectedOption].description : ""
+
     return (
       <View style={styles.container}>
-        <Text style={styles.titleText}>Hey {this.props.firstName}! Its time to get to work</Text>
-        {this.paymentOptions.map((option, idx) => {
-          return (
-            <SelectableCard
-                selected={this.state.selectedOption === idx}
-                onSelect={() => {
-                  this.setState({selectedOption: idx})
-                }}
-                {...option}
-                />
-          )
-        })}
-        <Button
-          onPress={this.openNativePurchaseOption}
-          title="Purchase">
-        </Button>
+        <View style={styles.titleTextContainer}>
+          <Text style={styles.titleText}>
+            Hey {this.props.firstName}!
+          </Text>
+          <Text style={styles.titleSubtext}>
+            Its time to get to work
+          </Text>
+        </View>
+        <View style={styles.optionsSelectableContainer}>
+          {this.paymentOptions.map((option, idx) => {
+            return (
+              <SelectableCard
+                  selected={this.state.selectedOption === idx}
+                  onSelect={() => {
+                    this.setState({selectedOption: idx})
+                  }}
+                  {...option}
+                  />
+            )
+          })}
+        </View>
+        <View style={styles.optionsExecuteContainer}>
+          <View style={styles.descriptionContainer}>
+            <Text>{description}</Text>
+          </View>
+          <Button
+            onPress={this.openNativePurchaseOption}
+            title="Purchase">
+          </Button>
+        </View>
+
       </View>
     )
   }
@@ -75,7 +105,38 @@ class PaymentScreen extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    backgroundColor: "#000000"
+  },
+  titleTextContainer: {
+    paddingLeft: Layout.window.width * .03,
+  },
+    titleText: {
+      color: "#ffffff",
+      fontSize: 16
+    },
+    titleSubtext: {
+      color: "#ffffff",
+    },
+  optionsSelectableContainer: {
+    position: "relative",
+    marginHorizontal: Layout.window.width *.02,
+    zIndex: 2,
+    flex: 2,
+    backgroundColor: "#000000",
+    flexDirection: "row",
+    justifyContent: "space-around"
+  },
+  optionsExecuteContainer: {
+    flex: 3,
+    zIndex: 1,
+    position: "relative",
+    borderWidth: 1,
+    backgroundColor: "#F2F2F2",
+    paddingTop: 70,
+    borderTopRightRadius: 40,
+    borderTopLeftRadius: 40,
+    alignItems: "center"
   }
 })
 
