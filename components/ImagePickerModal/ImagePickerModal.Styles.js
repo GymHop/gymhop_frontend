10ac87e8927f733878baa019a9ffa910f9b7e5fe
shoaib2527 <36672933+shoaib2527@users.noreply.github.com>
2021-03-25 20:47:26 +0500

    Image Upload Issue Resolved
    Engineer007

---
+++ components/ImagePickerModal/ImagePickerModal.Styles.js
@@ -0,0 +1,42 @@
+
+import { StyleSheet } from 'react-native';
+import { width, height, totalSize } from 'react-native-dimension';
+
+const styles = StyleSheet.create({
+    modalContainer: {
+        backgroundColor: 'white',
+        width: width(70),
+        borderRadius: 20,
+        alignSelf: 'center',
+        paddingBottom: height(3)
+    },
+    closeContainer: {
+        width: width(70),
+        alignSelf: 'center',
+        alignItems: 'flex-end',
+        marginTop: width(4),
+        justifyContent: 'space-between',
+        paddingHorizontal: width(4)
+    },
+    buttonContainer: {
+        width: width(70),
+        height: height(15),
+        justifyContent: 'space-evenly',
+    },
+    picOption: {
+        paddingLeft: width(12),
+        alignItems: 'center',
+        flexDirection: 'row'
+    },
+    picOptionText: {
+        marginLeft: width(2),
+        fontSize: totalSize(2)
+    },
+    line: {
+        width: '70%',
+        alignSelf: 'center',
+        backgroundColor: 'gray',
+        height: 0.5
+    }
+});
+export default styles;
